55d238e4e9ea888f9d46328b2e0f216c20da4e95
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-11-01 17:40:36 +0200

    Delete serving static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client/build'),
-    }),
-    TasksModule,
-  ],
+  imports: [TasksModule],
 
   controllers: [AppController],
 
