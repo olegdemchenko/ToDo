20dae4b488222e50b74039ac670c88cb41cabdab
Oleh Demchenko <olegdemchenko1993@gmail.com>
2023-01-03 12:31:28 +0200

    Fix extracting user data from request in login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,18 +2,18 @@ import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(AuthGuard('login'))
-  @Post('auth/login')
+  @Post('login')
   async login(@Request() req: any) {
-    return this.authService.login(req.user);
+    return this.authService.login(req.user._doc);
   }
 
   @UseGuards(AuthGuard('signup'))
-  @Post('auth/signup')
+  @Post('signup')
   async signup(@Request() req: any) {
     return this.authService.signUp(req.user);
   }
